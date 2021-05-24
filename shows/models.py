136d72f6be7701bcefc09792590821bcd22845b6
Your Name <stevenbeltz@ymail.com>
2021-05-24 11:34:15 -0500

    added validation

--- shows/models.py
+++ shows/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Error, Title too short."
+        if len(postData['network']) < 3:
+            errors['network'] = "Error, Network name too short."
+        if len(postData['desc']) < 10:
+            errors['desc'] = "Error, Description length too short."
+
 class Network(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,4 +23,4 @@ class Show(models.Model):
     network = models.ForeignKey(Network, related_name="show", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    objects = ShowManager()
--- shows/views.py
+++ shows/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import *
 
 def index(request):
@@ -20,14 +21,21 @@ def create(request):
     if request.method == "GET":
         return render(request,'new_show.html')
     else:
-        new_title = request.POST['title']
-        network = request.POST['network']
-        new_date = request.POST['date']
-        new_desc = request.POST['desc']
-        new_network = Network.objects.create(name=network)
+        errors = Show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for k, v in errors.items():
+                messages.error(request, v)
+            return redirect('/')
+        else:
+            new_title = request.POST['title']
+            network = request.POST['network']
+            new_date = request.POST['date']
+            new_desc = request.POST['desc']
+            new_network = Network.objects.create(name=network)
 
-        Show.objects.create(title=new_title, network=new_network, release_date=new_date, desc=new_desc)
-        return redirect('/')
+            Show.objects.create(title=new_title, network=new_network, release_date=new_date, desc=new_desc)
+            messages.success(request, "Show Creation Successful!")
+            return redirect('/')
 
 def edit(request, show_id):
     show = Show.objects.get(id=show_id)
@@ -37,17 +45,24 @@ def edit(request, show_id):
         }
         return render(request,'edit.html',context)
     else:
-        title = request.POST['title']
-        network = request.POST['network']
-        date = request.POST['date']
-        desc = request.POST['desc']
+        errors = Show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for k, v in errors.items():
+                messages.error(request, v)
+            return redirect('/')
+        else:
+            title = request.POST['title']
+            network = request.POST['network']
+            date = request.POST['date']
+            desc = request.POST['desc']
 
-        show.title = title
-        show.network.name = network
-        show.release_date = date
-        show.desc = desc
-        show.save()
-        return redirect(f'/shows/{show.id}')
+            show.title = title
+            show.network.name = network
+            show.release_date = date
+            show.desc = desc
+            show.save()
+            messages.success(request, "Show successfully updated!")
+            return redirect(f'/shows/{show.id}')
 
 def delete(request, show_id):
     show = Show.objects.get(id=show_id)
